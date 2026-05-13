5ad0aead3f0b65d036cab9fd1f787ae943199050
KikeAmado <eamadob23@fpcoruna.afundacion.org>
2026-05-13 14:42:04 +0200

    Actualización modelo Exercise con Serializers

--- app/src/main/java/com/example/gymappfronted/Models/Exercise.java
+++ app/src/main/java/com/example/gymappfronted/Models/Exercise.java
@@ -1,8 +1,22 @@
 package com.example.gymappfronted.Models;
+
+import com.google.gson.annotations.SerializedName;
+
 public class Exercise {
     private int id;
+    @SerializedName("nombre")
     private String name;
+
+    @SerializedName("musculo")
     private String body_part;
+
+    public Exercise() {}
+
+    public Exercise(int id, String name, String body_part) {
+        this.id = id;
+        this.name = name;
+        this.body_part = body_part;
+    }
 
     // Getters (Importante para que Android pueda leer los datos)
     public int getId() { return id; }
