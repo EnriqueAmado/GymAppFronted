59fb36d0322bf44dd5c12d2df49a0544dd990dbe
KikeAmado <eamadob23@fpcoruna.afundacion.org>
2026-05-20 14:46:09 +0200

    Corregido mapeo de JSON en Exercise.java

--- app/src/main/java/com/example/gymappfronted/Models/Exercise.java
+++ app/src/main/java/com/example/gymappfronted/Models/Exercise.java
@@ -3,23 +3,30 @@ package com.example.gymappfronted.Models;
 import com.google.gson.annotations.SerializedName;
 
 public class Exercise {
-    private int id;
-    @SerializedName("nombre")
+    private int id; // Si se llama igual en el JSON e igual en Java, no hace falta @SerializedName
+
+    @SerializedName("name") // Coincide con el campo de Django
     private String name;
 
-    @SerializedName("musculo")
-    private String body_part;
+    @SerializedName("description")
+    private String description;
 
+    @SerializedName("body_part") // Coincide con el campo de Django
+    private String bodyPart;
+
+    // Constructor vacío obligatorio para Retrofit
     public Exercise() {}
 
-    public Exercise(int id, String name, String body_part) {
+    public Exercise(int id, String name, String description, String bodyPart) {
         this.id = id;
         this.name = name;
-        this.body_part = body_part;
+        this.description = description;
+        this.bodyPart = bodyPart;
     }
 
-    // Getters (Importante para que Android pueda leer los datos)
+    // --- GETTERS ---
     public int getId() { return id; }
     public String getName() { return name; }
-    public String getBodyPart() { return body_part; }
+    public String getDescription() { return description; }
+    public String getBodyPart() { return bodyPart; }
 }
